45402ab064429f11f0c2f48e2a46ff7a4489af0b
gaoyingxu <374046758@qq.com>
2020-03-06 19:43:53 +0800

    增加antd组件

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ Component } from "react";
+import {Button,message} from "antd";
 import './App.css';
+/*
+应用的根组件
+*/
+ class App extends Component{
+  handleClick = ()=>{
+  message.success('点击正确');
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render(){
+    return (
+      <div>
+    <Button type="primary" onClick={this.handleClick}>app</Button>
+    <a>this</a>
+      </div>
+
+    
+    )
+  }
 }
-
-export default App;
+export default App
