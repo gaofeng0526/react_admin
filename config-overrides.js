d6198ad53b3c4865e6992afa9f3f3712af3cb594
gaoyingxu <374046758@qq.com>
2020-03-06 19:48:16 +0800

    增加配置文件

---
+++ config-overrides.js
@@ -0,0 +1,15 @@
+
+ const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+
+module.exports = override(
+  //针对antd实现按需打包，根据import来进行打包
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+   style: true,
+  }),
+ addLessLoader({
+   javascriptEnabled: true,
+   modifyVars: { '@primary-color': '#1DA57A' ,'@link-color': '#1890ff'},
+ }),
+);
